0c0149e9142cfb4ac5665f74c1ab6c74a0b02e6f
truongnm-0478 <ngo.mau.truong@sun-asterisk.com>
2024-09-21 15:04:01 +0700

    fix: Cors filter config

--- src/main/java/com/dut/pbl6_server/config/auth/CorsFilterConfig.java
+++ src/main/java/com/dut/pbl6_server/config/auth/CorsFilterConfig.java
@@ -1,21 +1,41 @@
 package com.dut.pbl6_server.config.auth;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class CorsFilterConfig implements Filter {
+
+    @Value("${application.api-key.header-name}")
+    private String apiKey;
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, Content-Type, Accept, " + apiKey);
         response.setHeader("Access-Control-Expose-Headers", "Location");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
@@ -27,3 +47,4 @@ public class CorsFilterConfig implements Filter {
     public void destroy() {
     }
 }
+
