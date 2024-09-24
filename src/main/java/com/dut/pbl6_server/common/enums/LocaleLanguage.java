c38f20286c58ceaa8ff8ca7ae34f24aeaef3b7d4
Thanh Vinh <ptvinh203@gmail.com>
2024-09-24 10:10:04 +0700

    feat: i18n

---
+++ src/main/java/com/dut/pbl6_server/common/enums/LocaleLanguage.java
@@ -0,0 +1,13 @@
+package com.dut.pbl6_server.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum LocaleLanguage {
+    VI("vi"),
+    EN("en");
+
+    private final String value;
+}
