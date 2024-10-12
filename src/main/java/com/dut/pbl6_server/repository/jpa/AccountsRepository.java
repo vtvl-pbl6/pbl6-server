c04bd076b83716ca23a23802b642a1e7d517642f
Phạm Thành Vinh <ptvinh203@gmail.com>
2024-10-12 23:13:10 +0700

    feat: done search, view, follow the user (#12)

--- src/main/java/com/dut/pbl6_server/repository/jpa/AccountsRepository.java
+++ src/main/java/com/dut/pbl6_server/repository/jpa/AccountsRepository.java
@@ -1,6 +1,8 @@
 package com.dut.pbl6_server.repository.jpa;
 
 import com.dut.pbl6_server.entity.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,4 +17,16 @@ public interface AccountsRepository extends JpaRepository<Account, Long> {
     boolean isAdministrator(Long id);
 
     boolean existsByDisplayName(String displayName);
+
+    @Query("""
+        SELECT a
+        FROM Account a
+        LEFT JOIN FETCH a.avatarFile
+        WHERE
+            LOWER(a.displayName) LIKE LOWER(CONCAT('%', :displayName, '%'))
+            AND a.role != 'ADMIN'
+            AND (a.visibility = 'PUBLIC' OR a.visibility = 'FRIEND_ONLY')
+            AND a.deletedAt IS NULL
+        """)
+    Page<Account> searchByDisplayName(String displayName, Pageable pageable);
 }
--- src/main/java/com/dut/pbl6_server/service/impl/AccountServiceImpl.java
+++ src/main/java/com/dut/pbl6_server/service/impl/AccountServiceImpl.java
@@ -2,12 +2,19 @@ package com.dut.pbl6_server.service.impl;
 
 import com.dut.pbl6_server.common.constant.ErrorMessageConstants;
 import com.dut.pbl6_server.common.exception.BadRequestException;
+import com.dut.pbl6_server.common.model.DataWithPage;
+import com.dut.pbl6_server.common.util.PageUtils;
 import com.dut.pbl6_server.dto.respone.AccountResponse;
 import com.dut.pbl6_server.entity.Account;
+import com.dut.pbl6_server.entity.Follower;
+import com.dut.pbl6_server.entity.enums.AccountRole;
 import com.dut.pbl6_server.mapper.AccountMapper;
 import com.dut.pbl6_server.repository.fetch_data.AccountsFetchRepository;
+import com.dut.pbl6_server.repository.jpa.AccountsRepository;
+import com.dut.pbl6_server.repository.jpa.FollowersRepository;
 import com.dut.pbl6_server.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,6 +23,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
     private final AccountsFetchRepository accountsFetchRepository;
+    private final AccountsRepository accountsRepository;
+    private final FollowersRepository followersRepository;
     private final AccountMapper accountMapper;
 
     @Override
@@ -30,4 +39,50 @@ public class AccountServiceImpl implements AccountService {
         }
     }
 
+    @Override
+    public AccountResponse getAccountInfoById(Account currentUser, Long userId) {
+        var account = accountsRepository.findById(userId).orElseThrow(() -> new BadRequestException(ErrorMessageConstants.ACCOUNT_NOT_FOUND));
+
+        // Check account's visibility
+        return switch (account.getVisibility()) {
+            case PUBLIC -> accountMapper.toUserResponse(account);
+            case PRIVATE -> throw new BadRequestException(ErrorMessageConstants.ACCOUNT_IS_NOT_AVAILABLE);
+            case FRIEND_ONLY -> {
+                if (followersRepository.isFollowing(userId, currentUser.getId()))
+                    yield accountMapper.toUserResponse(account);
+                else
+                    throw new BadRequestException(ErrorMessageConstants.ACCOUNT_IS_NOT_AVAILABLE);
+            }
+        };
+    }
+
+    @Override
+    public DataWithPage<AccountResponse> searchUser(Account currentUser, String displayName, Pageable pageable) {
+        var page = accountsRepository.searchByDisplayName(displayName, pageable);
+        return DataWithPage.<AccountResponse>builder()
+            .data(page.getContent().stream().map(accountMapper::toUserResponse).toList())
+            .pageInfo(PageUtils.makePageInfo(page))
+            .build();
+    }
+
+    @Override
+    public void followUser(Account currentUser, Long userId) {
+        var user = accountsRepository.findById(userId).orElseThrow(() -> new BadRequestException(ErrorMessageConstants.ACCOUNT_NOT_FOUND));
+        // Check if user is admin
+        if (user.getRole() == AccountRole.ADMIN)
+            throw new BadRequestException(ErrorMessageConstants.CANNOT_FOLLOW_ADMIN);
+        
+        // Check if user who is followed is current user
+        if (currentUser.getId().equals(userId))
+            throw new BadRequestException(ErrorMessageConstants.CANNOT_FOLLOW_YOURSELF);
+
+        // Check if user is already followed
+        if (followersRepository.isFollowing(userId, currentUser.getId()))
+            throw new BadRequestException(ErrorMessageConstants.ALREADY_FOLLOWED);
+
+        // Follow user (save to database)
+        followersRepository.save(new Follower(currentUser, user));
+
+        // TODO: send notification (via stomp websocket)
+    }
 }
